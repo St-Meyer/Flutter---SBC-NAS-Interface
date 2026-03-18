6cafef8f6beb0c89ba5b96fc5d517f0806b14e09
Steffen Meyer <Meyer.92@gmx.de>
2026-03-18 15:23:45 +0100

    Automatischen .venv einstieg deaktiviert und Netzwerktest via Dart

--- src/frontend/main.dart
+++ src/frontend/main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() async {
   var client = HttpClient();
-  var request = await client.getUrl(Uri.parse('http://192.168.0.33'));
+  var request = await client.getUrl(Uri.parse('http://192.168.0.33:8080'));
   var response = await request.close();
 
   if (response.statusCode == HttpStatus.ok) {
