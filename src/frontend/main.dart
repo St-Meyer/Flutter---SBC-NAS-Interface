4fac4407f638f45049e6058ebec8281d87ac3e58
Steffen Meyer <Meyer.92@gmx.de>
2026-03-18 14:38:20 +0100

    Neue Ordnerstruktur -> Frontend und Backend

---
+++ src/frontend/main.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() async {
+  var client = HttpClient();
+  var request = await client.getUrl(Uri.parse('http://192.168.0.33'));
+  var response = await request.close();
+
+  if (response.statusCode == HttpStatus.ok) {
+    print("Response code successful");
+  }
+  else {
+    print("Fehler");
+  }
+}
