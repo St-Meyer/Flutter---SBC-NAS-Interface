79b8215ba096b1aa73147bcfc5655af0a48c7f09
Steffen Meyer <Meyer.92@gmx.de>
2026-03-26 14:14:51 +0100

    connection.dart in OOP geschrieben und in main aufgerufen.

---
+++ src/frontend/connection.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+class Connection {
+  // Deklaration
+  String ip;
+  String port;
+
+  // Konstruktor
+  Connection(this.ip, this.port);
+
+  // Methode für Verbindungsaufbau, Bool-Ausgabe ob gelungen oder nicht
+  Future<bool> dial() async {
+    var client = HttpClient();
+    var request = await client.getUrl(Uri.parse("http://" + ip + ":" + port));
+    var response = await request.close();
+
+    if (response.statusCode == HttpStatus.ok) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
--- src/frontend/main.dart
+++ src/frontend/main.dart
@@ -1,14 +1,8 @@
-import 'dart:io';
+import 'connection.dart';
 
 void main() async {
-  var client = HttpClient();
-  var request = await client.getUrl(Uri.parse('http://192.168.0.33:8080'));
-  var response = await request.close();
-
-  if (response.statusCode == HttpStatus.ok) {
-    print("Response code successful");
-  }
-  else {
-    print("Failure");
-  }
+  String ip = "192.168.0.33";
+  String port = "8080";
+  var connection = Connection(ip, port);
+  bool connectionDial = await connection.dial();
 }
