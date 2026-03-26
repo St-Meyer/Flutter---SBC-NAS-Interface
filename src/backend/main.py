c43cb3378eccb35f3f7c993c0acb5e1600857f6a
Steffen Meyer <Meyer.92@gmx.de>
2026-03-26 15:51:40 +0100

    psutil in API angebungen, für erste Funktionstests

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+import psutil
 
 app = FastAPI()
 
 @app.get("/status")
 async def status():
-    return {"message": "Hello world"}
+    return {"CPU AUSLASTUNG:": str(psutil.cpu_percent(interval=1, percpu=True))}
