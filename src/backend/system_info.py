6deced9ee52a45004a22b380e5148b509269127f
Steffen Meyer <Meyer.92@gmx.de>
2026-04-08 14:30:55 +0200

    weitere psutil Systemabfragen in SystemInfo()

--- src/backend/system_info.py
+++ src/backend/system_info.py
@@ -5,5 +5,42 @@ class SystemInfo():
     def status(self):
         return {"status": "ok"}
     
+    # CPU Utilization
     def cpuPercents(self):
         return psutil.cpu_percent(interval=1, percpu=True)
+    
+    # CPU Frequency
+    def cpuFrequency(self):
+        return psutil.cpu_freq(percpu=True)
+    
+    # CPU Temperatures
+    def cpuTemperatures(self):
+        return psutil.sensors_temperatures()
+    
+    # RAM Usage
+    def ramUsage(self):
+        return psutil.virtual_memory()
+    
+    # SSD Usage
+    def diskUsage(self):
+        return psutil.disk_usage('/')
+    
+    # SSD-I/O
+    def ssdIO(self):
+        return psutil.disk_io_counters(perdisk=True)
+    
+    # Network Connections
+    def networkIO(self):
+        return psutil.net_io_counters(pernic=True)
+    
+    # Actice Network Connections
+    def networkConnections(self):
+        return psutil.net_connections()
+    
+    # System Boot Time
+    def bootTime(self):
+        return psutil.boot_time()
+    
+    # Running Processes
+    def processes(self):
+        return psutil.process_iter()
