af1d5a05a86ad0e3b399fabdd9f044bfbc4d666e
Steffen Meyer <Meyer.92@gmx.de>
2026-04-16 12:46:17 +0200

    runApp in main, parameterübergabe der ip und des ports in LandingPage

--- src/frontend/landing_page.dart
+++ src/frontend/landing_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'connection.dart';
+import 'api_reader.dart';
+import 'main.dart';
 
 class LandingPage extends StatefulWidget {
-  const LandingPage({super.key});
+  String ip;
+  String port;
+
+  LandingPage({super.key, required this.ip, required this.port});
+
+  var connection = Connection(ip, port);
+  var apiReader = ApiReader(connection);
 
   @override
   State<LandingPage> createState() => _LandingPageState();
@@ -11,8 +20,11 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     const String appTitle = "SBC-NAS Interface";
-    return AppBar(
-      title: appTitle,
+    return Scaffold(
+      appBar: AppBar(title: const Text(appTitle)),
+      body: const Center(
+        child: Text("Hello World"),
+      ),
     );
   }
 }
--- src/frontend/main.dart
+++ src/frontend/main.dart
@@ -1,13 +1,16 @@
 import 'connection.dart';
 import 'api_reader.dart';
+import 'landing_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
   String ip = dotenv.env['IP_ADDRESS'].toString();
   String port = dotenv.env['PORT'].toString();
-  var connection = Connection(ip, port);
-  var apiReader = ApiReader(connection);
+
+  LandingPage(ip: ip, port: port);
+
   bool connectionDial = await connection.dial();
 
   if (connectionDial) {
@@ -15,4 +18,6 @@ void main() async {
   } else {
     print("Connection Failure");
   }
+
+  runApp(const LandingPage());
 }
