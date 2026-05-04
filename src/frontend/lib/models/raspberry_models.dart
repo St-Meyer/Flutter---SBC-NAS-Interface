64b787819e76874db9df750101ca32e69a2ced10
Steffen Meyer <Meyer.92@gmx.de>
2026-05-04 11:22:30 +0200

    Container für Raspberry Modells erstellt.

---
+++ src/frontend/lib/models/raspberry_models.dart
@@ -0,0 +1,15 @@
+class RaspberryModels {
+   int id;
+   String modelName;
+   String ramModel;
+   double idlePower;
+   double nasPower;
+
+   RaspberryModels(
+    this.id, 
+    this.modelName, 
+    this.ramModel, 
+    this.idlePower, 
+    this.nasPower
+  );
+}
