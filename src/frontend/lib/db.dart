057856b2b655454cfa757edd4db8807556935d3c
Steffen Meyer <Meyer.92@gmx.de>
2026-05-05 13:59:13 +0200

    Datenbankverbindung als Singleton Pattern erstellt.

--- src/frontend/lib/db.dart
+++ src/frontend/lib/db.dart
@@ -1,3 +1,13 @@
-class Database {
+import 'package:sqflite/sqflite.dart';
+
+class Db {
   
+// Singleton Pattern - Damit die Datenbankverbindung nur einmal aufgebaut wird.
+
+    static final Db _instance = Db._internal();
+
+    factory Db() => _instance;
+
+    Db._internal();
+
 }
