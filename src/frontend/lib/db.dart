a3860cb3b2b9a80c4a74dd06377226928611ff34
Steffen Meyer <Meyer.92@gmx.de>
2026-05-06 10:38:38 +0200

    2. SQL Query erstellt

--- src/frontend/lib/db.dart
+++ src/frontend/lib/db.dart
@@ -19,8 +19,10 @@ class Db {
             "database.db");
         Database database = await openDatabase(dbPath, version: 1, onCreate: (Database db, int version) async {
             await db.execute(
-                'CREATE TABLE raspberry_models(id INTEGER Primary Key, name TEXT, rammodel TEXT, idlepower REAL, naspower REAL);');
-        });
-        return database;
+                'CREATE TABLE IF NOT EXISTS raspberry_models(id INTEGER Primary Key, modelname TEXT, rammodel TEXT, idlepower REAL, naspower REAL);');
+            await db.execute(
+                'CREATE TABLE IF NOT EXISTS settings(id INTEGER Primary Key, ip TEXT, port TEXT, kwhpreis REAL, modelname TEXT, FOREIGN KEY(modelname) REFERENCES raspberry_models(modelname));');
+            });
+                return database;
     }
 }
