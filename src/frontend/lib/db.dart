9279d1e55435e1075711163f41e531e6e451cd81
Steffen Meyer <Meyer.92@gmx.de>
2026-05-06 10:12:03 +0200

    Öffnen der Datenbank und erstellen einer Tabelle in dieser Datanbank, sollte sie das 1. Mal geöffnet werden.

--- src/frontend/lib/db.dart
+++ src/frontend/lib/db.dart
@@ -1,4 +1,5 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class Db {
   
@@ -10,4 +11,16 @@ class Db {
 
     Db._internal();
 
+    late Database db;
+    
+    Future <Database> init() async {
+        String dbPath = join(
+            await getDatabasesPath(),
+            "database.db");
+        Database database = await openDatabase(dbPath, version: 1, onCreate: (Database db, int version) async {
+            await db.execute(
+                'CREATE TABLE raspberry_models(id INTEGER Primary Key, name TEXT, rammodel TEXT, idlepower REAL, naspower REAL);');
+        });
+        return database;
+    }
 }
