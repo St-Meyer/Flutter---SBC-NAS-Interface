417a80480a2d277a47302a092c0eeaf0c56fa87a
Steffen Meyer <Meyer.92@gmx.de>
2026-05-06 12:32:38 +0200

    Weitere Tabellen ersteööt für 3. Normalform

--- src/frontend/lib/db.dart
+++ src/frontend/lib/db.dart
@@ -14,13 +14,26 @@ class Db {
     late Database db;
 
                 
-    String modelquery = 'CREATE TABLE IF NOT EXISTS raspberry_models('
-                        'id INTEGER Primary Key, '
-                        'modelname TEXT, '
-                        'rammodel TEXT, '
+    String modeltable = 'CREATE TABLE IF NOT EXISTS raspberry_models('
+                        'id INTEGER Primary Key Autoincrement, '
+                        'series_id INTEGER, '
+                        'ram_id INTEGER, '
                         'idlepower REAL, '
-                        'naspower REAL);';
-    String settingquery = 'CREATE TABLE IF NOT EXISTS settings('
+                        'naspower REAL,'
+                        'FOREIGN KEY (series_id)'
+                        'REFERENCES raspberry_series(id),'
+                        'FOREIGN KEY (ram_id) '
+                        'REFERENCES ram_models(id));';
+
+    String seriestable = 'CREATE TABLE IF NOT EXISTS raspberry_series('
+                            'id INTEGER Primary Key Autoincrement, '
+                            'seriesname TEXT)';
+
+    String ramtable = 'CREATE TABLE IN NOT EXISTS ram_models('
+                        'id INTEGER Primary Key Autoincrement, '
+                        'ramsize text)';
+
+    String settingstable = 'CREATE TABLE IF NOT EXISTS settings('
                             'id INTEGER Primary Key, '
                             'ip TEXT, '
                             'port TEXT, '
@@ -28,7 +41,11 @@ class Db {
                             'modelname TEXT, '
                             'FOREIGN KEY(modelname) '
                             'REFERENCES raspberry_models(modelname));';
-    String seeddata = "";
+                            
+    String seeddataModel = 'INSERT INTO raspberry_models(modelname, '
+                            'ram_id, '
+                            'idlepower, '
+                            'naspower)';
 
     
     Future <Database> init() async {
@@ -36,9 +53,9 @@ class Db {
             await getDatabasesPath(),
             "database.db");
         Database database = await openDatabase(dbPath, version: 1, onCreate: (Database db, int version) async {
-            await db.execute(modelquery);
-            await db.execute(settingquery);
-            await db.execute(seeddata);
+            await db.execute(modeltable);
+            await db.execute(settingstable);
+            await db.execute(seeddataModel);
             });
                 return database;
     }
