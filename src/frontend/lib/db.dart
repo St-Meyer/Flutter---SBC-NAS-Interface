6efbfa5c87883651697f2cc864390b5ef785c34b
Steffen Meyer <Meyer.92@gmx.de>
2026-05-06 10:47:40 +0200

    SQL Befehle in  String Variablen ausgelagert.

--- src/frontend/lib/db.dart
+++ src/frontend/lib/db.dart
@@ -12,16 +12,33 @@ class Db {
     Db._internal();
 
     late Database db;
+
+                
+    String modelquery = 'CREATE TABLE IF NOT EXISTS raspberry_models('
+                        'id INTEGER Primary Key, '
+                        'modelname TEXT, '
+                        'rammodel TEXT, '
+                        'idlepower REAL, '
+                        'naspower REAL);';
+    String settingquery = 'CREATE TABLE IF NOT EXISTS settings('
+                            'id INTEGER Primary Key, '
+                            'ip TEXT, '
+                            'port TEXT, '
+                            'kwhpreis REAL, '
+                            'modelname TEXT, '
+                            'FOREIGN KEY(modelname) '
+                            'REFERENCES raspberry_models(modelname));';
+    String seeddata = "";
+
     
     Future <Database> init() async {
         String dbPath = join(
             await getDatabasesPath(),
             "database.db");
         Database database = await openDatabase(dbPath, version: 1, onCreate: (Database db, int version) async {
-            await db.execute(
-                'CREATE TABLE IF NOT EXISTS raspberry_models(id INTEGER Primary Key, modelname TEXT, rammodel TEXT, idlepower REAL, naspower REAL);');
-            await db.execute(
-                'CREATE TABLE IF NOT EXISTS settings(id INTEGER Primary Key, ip TEXT, port TEXT, kwhpreis REAL, modelname TEXT, FOREIGN KEY(modelname) REFERENCES raspberry_models(modelname));');
+            await db.execute(modelquery);
+            await db.execute(settingquery);
+            await db.execute(seeddata);
             });
                 return database;
     }
