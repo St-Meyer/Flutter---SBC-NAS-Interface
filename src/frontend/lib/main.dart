5a84f146d82398cd3f3395f659fa6859e1033cfa
Steffen Meyer <Meyer.92@gmx.de>
2026-05-07 08:54:15 +0200

    Db.init() in main.dart hinzugefügt.

--- src/frontend/lib/main.dart
+++ src/frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'landing_page.dart';
+import 'db.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,8 @@ void main() async {
   String ip = dotenv.env['IP_ADDRESS'].toString();
   String port = dotenv.env['PORT'].toString();
 
+  // Datenbank-Initialisierung
+  Db().init;
   // IP und Port werden aus .env entnommen und in LandingPage
   // als Parameter weiter gegeben
   runApp(LandingPage(ip: ip, port: port));
