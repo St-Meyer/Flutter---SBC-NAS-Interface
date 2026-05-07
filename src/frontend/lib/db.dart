f079f72538547b4532914b8448db7f452dfb5891
Steffen Meyer <Meyer.92@gmx.de>
2026-05-07 08:52:14 +0200

    Seed Date für die Modelle erstellt und db.execute erweitert.

--- src/frontend/lib/db.dart
+++ src/frontend/lib/db.dart
@@ -43,29 +43,48 @@ class Db {
                             'REFERENCES raspberry_models(id));';
 
     String seeddataSeries = 'INSERT INTO raspberry_series('
-                            'seriesname) VALUES'
-                            '("Pi Zero 2 W"),'
-                            '("Pi 3B+"),'
-                            '("Pi 4B"),'
-                            '("Pi 5");';
+                            'seriesname) VALUES '
+                            '("Pi Zero 2 W"), '
+                            '("Pi 3B+"), '
+                            '("Pi 4B"), '
+                            '("Pi 5"); ';
 
-        String seeddataRam = 'INSERT INTO ram_models('
-                            'ramsize) VALUES'
-                            '("1GB"),'
-                            '("2GB"),'
-                            '("4GB"),'
-                            '("8GB"),'
+    String seeddataRam = 'INSERT INTO ram_models('
+                            'ramsize) VALUES '
+                            '("512MB"), '
+                            '("1GB"), '
+                            '("2GB"), '
+                            '("4GB"), '
+                            '("8GB"), '
                             '("16GB");';
 
+    String seeddataModels = 'INSERT INTO raspberry_models('
+                            'series_id, '
+                            'ram_id, '
+                            'idlepower, '
+                            'naspower) VALUES '
+                            '(1, 1, 0.4, 1.5), '
+                            '(2, 2, 1.9, 3.5), '
+                            '(3, 3, 2.7, 4.5), '
+                            '(3, 4, 2.9, 4.8), '
+                            '(3, 5, 3.0, 5.0), '
+                            '(4, 3, 2.2, 4.5), '
+                            '(4, 4, 2.7, 5.5), '
+                            '(4, 5, 3.0, 6.0), '
+                            '(4, 6, 3.2, 6.5);';
     
     Future <Database> init() async {
         String dbPath = join(
             await getDatabasesPath(),
             "database.db");
         Database database = await openDatabase(dbPath, version: 1, onCreate: (Database db, int version) async {
+            await db.execute(seriestable);
+            await db.execute(ramtable);
             await db.execute(modeltable);
             await db.execute(settingstable);
             await db.execute(seeddataSeries);
+            await db.execute(seeddataRam);
+            await db.execute(seeddataModels);
             });
                 return database;
     }
