ba88335e7d32120296c07a97d5ecfc02b1cab9d0
Steffen Meyer <Meyer.92@gmx.de>
2026-05-11 11:40:21 +0200

    Methode, um zu prüfen, ob  Einstellungen bereits gespeichert sind. Settings.Page.dart angelegt.

--- src/frontend/lib/main.dart
+++ src/frontend/lib/main.dart
@@ -2,15 +2,29 @@ import 'landing_page.dart';
 import 'db.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
   String ip = dotenv.env['IP_ADDRESS'].toString();
   String port = dotenv.env['PORT'].toString();
 
+
   // Datenbank-Initialisierung
   await Db().init();
   // IP und Port werden aus .env entnommen und in LandingPage
   // als Parameter weiter gegeben
   runApp(LandingPage(ip: ip, port: port));
+}
+
+
+
+Future <bool> checkSettings() async {
+  var x = await Db().db.query('settings');
+  if (x.isEmpty) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
