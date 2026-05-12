687f6c0d0de887a96b9cde616791c9d71c1edbcd
Steffen Meyer <Meyer.92@gmx.de>
2026-05-12 16:20:57 +0200

    settings_page bis IP Eingabe erstellt.

--- src/frontend/lib/settings_page.dart
+++ src/frontend/lib/settings_page.dart
@@ -10,7 +10,47 @@ class _SettingsPageState extends State<SettingsPage>{
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    const String settingsTitle = "Settings";
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: AlignmentGeometry.topCenter,
+                end: AlignmentGeometry.bottomCenter,
+                colors: [
+                  const Color.fromARGB(255, 47, 47, 47),
+                  Colors.white
+                ]
+              )
+            ),
+          ),
+          title: Text(settingsTitle),
+          centerTitle: true,
+                    titleTextStyle: TextStyle(
+              shadows: [Shadow(color: Colors.black, blurRadius: 4.0)],
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 255, 255, 255),
+              fontSize: 24),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Card(
+                color: Color.fromARGB(126, 163, 163, 163),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const Text("IP:"),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
